f77414ad49b9478d52e2f205c9efaaf24871cd33
mauricio.selfene <mauricio.selfene@mundotelecomunicaciones.cl>
2022-09-23 01:40:58 -0300

    implementacion de JWT

--- config/app.php
+++ config/app.php
@@ -182,6 +182,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
+
         /*
          * Package Service Providers...
          */
@@ -209,6 +212,11 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
+
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
